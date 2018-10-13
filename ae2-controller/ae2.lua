4758365fc1468715e8abd352565dd0a9c4e5911f
Simon Bastien-Filiatrault <root@gopoi.net>
2018-10-13 17:15:54 -0400

    imported me test

--- ae2-controller/ae2.lua
+++ ae2-controller/ae2.lua
@@ -1,2 +1,18 @@
 local Class = dofile("Class.lua")
 
+local component = require("component")
+local unserialize = require("serialization").unserialize
+local config = {}
+
+{
+   config = unserialize(io.open('/etc/ae2-controller.cfg'))
+}
+
+if component.isAvailable('me_interface') then
+   meIface = component.me_interface
+elseif component.isAvailable('me_controller') then
+   meIface = component.me_controller
+else
+   error("No available ME network")
+end
+
