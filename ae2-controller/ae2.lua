ab13b81f61a589e2806b1552b6f0d144f815c52e
Simon Bastien-Filiatrault <root@gopoi.net>
2018-10-14 15:24:54 -0400

    Done Item class with minimum functionnaliy

--- ae2-controller/ae2.lua
+++ ae2-controller/ae2.lua
@@ -5,7 +5,7 @@ local unserialize = require("serialization").unserialize
 local config = {}
 
 do --lazy file ref close
-   config = unserialize(io.open('/etc/ae2-controller.cfg'))
+   config = unserialize(io.open('/etc/ae2-controller.cfg').read('*a'))
 end
 
 if component.isAvailable('me_interface') then
@@ -16,3 +16,45 @@ else
    error("No available ME network")
 end
 
+local Item = {}
+Class(Item)
+
+function Item:constructor(itemLabel, itemTargetQty)
+   self.itemLabel = itemLabel
+   self.itemTargetQty = itemTargetQty
+   self.orderQtyAmmount = math.floor(self.itemTargetQty * 0.2)
+   self.iscraftable = false --false until proven otherwise
+   self.tracker = {
+      isDone = function() return true end}
+   self:getCurrentQty()
+
+end
+
+function Item:getCurrentQty()
+   local result = meIface.getItemsInNetwork({label=self.itemLabel})
+   if result['n'] >= 1 then
+      self.isCraftable = result[1].isCraftable
+      return result[1].size
+   else
+      io.stderr:write('Item not present and is not craftable: ' .. self.itemLabel)
+      self.isCraftable = false
+      return -1
+   end
+end
+
+function Item:requestItem()
+   if self.tracker.isDone() or self.tracker.isCanceled() then
+      local result = meIface.getCraftables({label=self.itemLabel})
+      if result['n'] >= 1 then 
+	 self.tracker = result[1].request(self.orderQtyAmmount)
+      else
+	 io.stderr:write('Item not present or not craftable: ' .. self.itemLabel
+			    .. '\n Skipping...')
+	 self.isCraftable = false
+      end
+   else
+      io.stderr:write('Item are still being crafted: ' .. self.iemLabel)
+   end
+end
+
+      
